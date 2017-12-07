db91fe7f7b1016ee14db1db6c824ca75eb73b4fe
Perry Fustero <pjfust@gmail.com>
2017-12-06 22:45:41 -0500

    add solution with bonus

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,19 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor() {
+    super()
+    this.state = {
+      counter: 0,
+      interval: null
+    }
+  }
+
+  startTimer() {
+    if (!this.state.interval) {
+      this.setState({ interval: setInterval(() => {
+        this.setState({
+          counter: this.state.counter + 1
+        })
+      }, 1000)})
+    }
+  }
+
+  pauseTimer() {
+    clearInterval(this.state.interval)
+    this.setState({
+      interval: null
+    })
+  }
+
+  resetTimer() {
+    this.setState({
+      counter: 0
+    })
+  }
+
   render() {
+    var zero = ''
+    var display = ''
+    if (this.state.counter === 0) {
+      zero = "red"
+      display = 'none'
+    }
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1 style={{color: zero}}>{this.state.counter}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button style={{display: display}} onClick={() => this.resetTimer()}>Reset</button>
+          <button onClick={() => this.startTimer()}>Start</button>
+          <button style={{display: display}} onClick={() => this.pauseTimer()}>Pause</button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Stopwatch;
+export default Stopwatch
